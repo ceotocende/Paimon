077df6ce488f314864df0e8294b56e6da3fd6562
Серов Андрей <andreyserov03@gmail.com>
2023-05-21 22:03:50 +0300

    startEvents edit

--- src/commands/other/startEvents.js
+++ src/commands/other/startEvents.js
@@ -24,53 +24,31 @@ module.exports = {
 
         const row = new ActionRowBuilder().addComponents(button)
 
-
         const c = cron.schedule('* * * * *', async () => {
             eventsCounter += 1;
-            usersMessages.sync();
-            const maxNumber = await usersMessages.max('users_message_timely');
-            const recordWithMaxNumber = await usersMessages.findOne({ where: { users_message_timely: maxNumber } });
-            console.log(maxNumber, recordWithMaxNumber)
-            channelTestBot.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setAuthor({ name: 'Кол-во сообщений' })
-                        .setDescription(`
-                        Недельный ивент подсчет сообщений! 
-                        Больше всего сообщений у <@${recordWithMaxNumber.user_id}> 
-                        кол-во сообщений ${recordWithMaxNumber.users_message_timely}
-                    `)
-                        .setTimestamp()
-                ],
-            })
         });
         c;
 
-
-        const updateJob = cron.schedule('* * * * *', async () => {
-            usersMessages.sync();
-            const maxNumber = await usersMessages.max('user_message');
-            const recordWithMaxNumber = await usersMessages.findOne({ where: { users_message_timely: maxNumber } });
-            channel.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setAuthor({ name: 'Кол-во сообщений' })
-                        .setDescription(`
-                        Недельный ивент подсчет сообщений! 
-                        Больше всего сообщений у <@${recordWithMaxNumber.user_id}> 
-                        кол-во сообщений ${recordWithMaxNumber.users_message_timely}
-                    `)
-                        .setTimestamp()
-                ],
-            })
-        });
-        updateJob;
-
-
-
         if (eventStart === 0) {
             eventStart += 1;
-         
+            const updateJob = cron.schedule('* * * * *', async () => {
+                usersMessages.sync();
+                const maxNumber = await usersMessages.max('user_message');
+                const recordWithMaxNumber = await usersMessages.findOne({ where: { users_message_timely: maxNumber } });
+                channel.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setAuthor({ name: 'Кол-во сообщений' })
+                            .setDescription(`
+                            Недельный ивент подсчет сообщений! 
+                            Больше всего сообщений у <@${recordWithMaxNumber.user_id}> 
+                            кол-во сообщений ${recordWithMaxNumber.users_message_timely}
+                        `)
+                            .setTimestamp()
+                    ],
+                })
+            });
+            updateJob;         
         } else {
             channelTestBot.send({
                 content: `Системное сообщение: ивент "Подсчет сообщений уже запущен"`
